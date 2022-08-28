4de6439cd4784c87fb77676af389adbe633ee53c
Samyuktha Vipin <samyvip@ad.unc.edu>
2022-08-27 21:29:24 -0400

    fixed gradescope errors

--- exercises/ex01_final_chardle.py
+++ exercises/ex01_final_chardle.py
@@ -18,38 +18,34 @@ print("Searching for " + single_character + " in " + five_character_word)
 
 if single_character == five_character_word[0]:
     character_count = character_count + 1
-    print( single_character + " found at index 0")
+    print(single_character + " found at index 0")
     
-
 
 if single_character == five_character_word[1]:
     character_count = character_count + 1
-    print( single_character + " found at index 1")
+    print(single_character + " found at index 1")
 
     
-
 if single_character == five_character_word[2]:
     character_count = character_count + 1
-    print( single_character + " found at index 2")
+    print(single_character + " found at index 2")
     
    
 if single_character == five_character_word[3]:
     character_count = character_count + 1
-    print( single_character + " found at index 3")
+    print(single_character + " found at index 3")
     
-
 
 if single_character == five_character_word[4]:
     character_count = character_count + 1
-    print( single_character + " found at index 4")
+    print(single_character + " found at index 4")
 
    
-
 if int(character_count) >= 2:
-    print( str(character_count) + " instances of " + str(single_character) + " found in " + str(five_character_word))  
+    print(str(character_count) + " instances of " + str(single_character) + " found in " + str(five_character_word))  
 
 if int(character_count) < 2:
     if int(character_count) == 0:
-        print( "No instances of " + str(single_character) + " found in " + str(five_character_word))
+        print("No instances of " + str(single_character) + " found in " + str(five_character_word))
     else:
-        print( str(character_count) + " instance of " + str(single_character) + " found in " + str(five_character_word))
+        print(str(character_count) + " instance of " + str(single_character) + " found in " + str(five_character_word))
