5c047b9d025d10f8c9dd63cc293759ace58a2fbe
Samyuktha Vipin <samyvip@ad.unc.edu>
2022-09-14 17:05:14 -0400

    Progress on Exercises 3

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -11,50 +11,58 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 if len(guess) == len(secret):
+    """Checks whether the length of the guess matches the length of the secret word."""
     while character_count < len(secret):
         if guess[character_count] == secret[character_count]:
             guess_emoji += GREEN_BOX
+            """Prints a green box if the the letter at the current index of the guessed word matches the letter at the same index of the secret word."""
         else:
             checking: bool = False
             alt_index: int = 0
-            while checking == False and character_count < len(secret) and alt_index < len(secret):
+            while checking is False and character_count < len(secret) and alt_index < len(secret):
                 if guess[character_count] == secret[alt_index]:
                     checking = True
                 else:
                     alt_index = alt_index + 1
-            if checking == True:
+            if checking is True:
                 guess_emoji += YELLOW_BOX
+                """Prints a yellow box if the letter at that index of the guessed word is in the secret word, but not in the same position."""
             else:
                 guess_emoji += WHITE_BOX
+                """Prints a white box if the letter at that index of the guessed word is not present at any index in the secret word."""
         character_count += 1
 else:
     length: bool = False
-    while len(guess) != len(secret) and length == False:
-        guess = input(f"That was not {len(secret)} letters! Try again: ")    
+    while len(guess) != len(secret) and length is False:
+        guess = input(f"That was not {len(secret)} letters! Try again: ") 
+        """Prompts the user to re-enter a word of the appropriate length."""   
         if len(guess) == len(secret):
-            length: bool = True
+            length = True
             while character_count < len(secret):
                 if guess[character_count] == secret[character_count]:
                     guess_emoji += GREEN_BOX
+                    """Prints a green box if the the letter at the current index of the guessed word matches the letter at the same index of the secret word."""
                 else:
-                    checking: bool = False
-                    alt_index: int = 0
-                    while checking == False and character_count < len(secret) and alt_index < len(secret):
+                    checking = False
+                    alt_index = 0
+                    while checking is False and character_count < len(secret) and alt_index < len(secret):
                         if guess[character_count] == secret[alt_index]:
                             checking = True
                         else:
                             alt_index += 1
-                    if checking == True:
+                    if checking is True:
                         guess_emoji += YELLOW_BOX
+                        """Prints a yellow box if the letter at that index of the guessed word is in the secret word, but not in the same position."""
                     else:
                         guess_emoji += WHITE_BOX
+                        """Prints a white box if the letter at that index of the guessed word is not present at any index in the secret word."""
                 character_count += 1
 
 print(guess_emoji)
+"""Prints resulting emoji string."""
 
 if guess == secret:
+    """Checks whether user has guessed the word correctly."""
     print("Woo! You got it!")
-    exit()
 else:
     print("Not quite. Play again soon!")
-    exit()
---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,70 @@
+"""EX03- Wordle."""
+
+__author__ = "730548773"
+
+WHITE_BOX: str = "\U00002B1C"
+YELLOW_BOX: str = "\U0001F7E8"
+GREEN_BOX: str = "\U0001F7E9"
+
+def contains_char(word: str, character: str) -> bool:
+    """Searches for character."""
+    assert len(character) == 1
+    checking: bool = False
+    word_index: int = 0
+    while checking is False and word_index < len(word):
+        if character == word[word_index]:
+            checking = True
+        else:
+            word_index = word_index + 1
+    if checking is True:
+        return True
+    else:
+        return False
+
+def emojified(guess: str, secret: str) -> str:
+    """Calls contains_char fn to test for colored box codification."""
+    assert len(guess) == len(secret)
+    guess_emoji: str = ""
+    word_index: int = 0
+    while word_index < len(guess):
+        if guess[word_index] == secret[word_index]:
+            guess_emoji += GREEN_BOX
+        else:
+            if contains_char(secret, guess[word_index]):
+                guess_emoji += YELLOW_BOX
+            else:
+                guess_emoji += WHITE_BOX
+        word_index += 1
+    return guess_emoji
+
+def input_guess(expected_length: int) -> bool:
+    """Prompts the user for a guess of the expected length."""
+    guess: str = input(f"Enter a {expected_length} character word: ")
+    if len(guess) == expected_length:
+        return guess
+    else:
+        while len(guess) != expected_length:
+            guess: str = input(f"That wasn't {expected_length} chars! Try again: ")
+            if len(guess) == expected_length:
+                return guess
+
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    secret: str = "codes"
+    turns: int = 1
+    success: bool = False
+    while turns <= 6 and success is False:
+        print(f"=== Turn {turns}/6 ===")
+        guess = input_guess(len(secret))
+        print(emojified(guess, secret))
+        if guess == secret:
+            success = True
+        else:
+            turns += 1
+    if guess == secret:
+        print(f"You won in {turns}/6 turns!")
+    else:
+        print("X/6 - Sorry, try again tomorrow!")
+
+if __name__ == "__main__":
+    main()
